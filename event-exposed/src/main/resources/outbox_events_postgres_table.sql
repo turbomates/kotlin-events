8d7dfdb9d48b8e51a97c64bef43798936c35ef85
Vadim Golodko <vadim@turbomates.com>
2025-10-30 00:04:35 +0100

    fix interceptor

--- event-exposed/src/main/resources/outbox_events_postgres_table.sql
+++ event-exposed/src/main/resources/outbox_events_postgres_table.sql
@@ -3,7 +3,6 @@
                 event jsonb NOT NULL,
                 created_at timestamp with time zone DEFAULT timezone('UTC'::text, statement_timestamp()) NOT NULL,
                 published_at timestamp with time zone,
-                traceparent TEXT,
-                span_id TEXT
+                trace_information jsonb NOT NULL
             );
 CREATE INDEX events_publisshed_idx ON outbox_events (published_at);
